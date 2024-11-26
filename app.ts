254b9aeced29c8b0720a2ca92363e945a9331339
Mulukuntla <yours.saisaketh@gmail.com>
2024-11-26 16:31:30 +0530

    arrays

--- app.ts
+++ app.ts
@@ -2,6 +2,9 @@ var num1Element=document.getElementById("num1") as HTMLInputElement;
 var num2Element=document.getElementById("num2") as HTMLInputElement;
 var  buttonElement=document.querySelector("button")!
 
+const numResults : number[]=[];
+const textResults : string[]=[];
+
 function add(num1: number | string,num2: number | string){
     if(typeof num1==="number" && typeof num2==="number"){
         return num1 + num2;
@@ -11,14 +14,21 @@ function add(num1: number | string,num2: number | string){
     }
     return +num1 + +num2
 }
+function printResult(resultObj : {val :number ; timestamp :Date}){
+    console.log(resultObj.val)
+    console.log(resultObj.timestamp)
+}
 
 buttonElement.addEventListener('click',()=>{
     var num1=num1Element.value;
     var num2=num2Element.value;
     var result=add(+num1,+num2);
+    numResults.push(result as number);
     var stringResult=add(num1,num2);
-    console.log(result);
-    console.log(stringResult);
+    textResults.push(stringResult as string);
+    
+    printResult({val:result as number ,timestamp :new Date()});
+    console.log(numResults,textResults)
 
 
 });
