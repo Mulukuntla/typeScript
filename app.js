04e3ba18cbe0f787aaede51ef2287e9de01baf8c
Mulukuntla <yours.saisaketh@gmail.com>
2024-11-27 10:17:15 +0530

    api typeScript

--- app.js
+++ app.js
@@ -1,37 +1,12 @@
 "use strict";
-var num1Element = document.getElementById("num1");
-var num2Element = document.getElementById("num2");
-var buttonElement = document.querySelector("button");
-const numResults = [];
-const textResults = [];
-function add(num1, num2) {
-    if (typeof num1 === "number" && typeof num2 === "number") {
-        return num1 + num2;
-    }
-    else if (typeof num1 === "string" && typeof num2 === "string") {
-        return num1 + " " + num2;
-    }
-    return +num1 + +num2;
-}
-function printResult(resultObj) {
-    console.log(resultObj.val);
-    console.log(resultObj.timestamp);
-}
-buttonElement.addEventListener('click', () => {
-    var num1 = num1Element.value;
-    var num2 = num2Element.value;
-    var result = add(+num1, +num2);
-    numResults.push(result);
-    var stringResult = add(num1, num2);
-    textResults.push(stringResult);
-    printResult({ val: result, timestamp: new Date() });
-    console.log(numResults, textResults);
-});
-const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("It worked !");
-    }, 1000);
-});
-myPromise.then((result) => {
-    console.log(result.split("w"));
-});
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const todos_1 = __importDefault(require("./routes/todos"));
+const app = (0, express_1.default)();
+app.use(body_parser_1.default.json());
+app.use(todos_1.default);
+app.listen(4008, () => console.log("Server running on port 4008"));
--- app.ts
+++ app.ts
@@ -1,52 +1,9 @@
-var num1Element=document.getElementById("num1") as HTMLInputElement;
-var num2Element=document.getElementById("num2") as HTMLInputElement;
-var  buttonElement=document.querySelector("button")!
-
-const numResults: number[] = [];
-const textResults: string[] = [];
-
-type NumOrString=number | string;
-type Result={val :number ; timestamp :Date}
-interface ResultObj{
-    val:number;
-    timestamp:Date;
-
-}
-
-function add(num1:NumOrString,num2:NumOrString){
-    if(typeof num1==="number" && typeof num2==="number"){
-        return num1 + num2;
-    }
-    else if(typeof num1==="string" && typeof num2==="string"){
-        return num1 +" "+ num2;
-    }
-    return +num1 + +num2
-}
-function printResult(resultObj : ResultObj){
-    console.log(resultObj.val)
-    console.log(resultObj.timestamp)
-}
-
-buttonElement.addEventListener('click',()=>{
-    var num1=num1Element.value;
-    var num2=num2Element.value;
-    var result=add(+num1,+num2);
-    numResults.push(result as number);
-    var stringResult=add(num1,num2);
-    textResults.push(stringResult as string);
-    
-    printResult({val:result as number ,timestamp :new Date()});
-    console.log(numResults,textResults)
+import express from "express";
+import bodyParser from "body-parser"
+import todosRoutes from "./routes/todos";
 
 
-});
-
-const myPromise=new Promise<string>((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve("It worked !")
-    },1000)
-
-})
-myPromise.then((result) =>{
-    console.log(result.split("w"))
-})
+const app=express();
+app.use(bodyParser.json())
+app.use(todosRoutes)
+app.listen(4008, () => console.log("Server running on port 4008"));
